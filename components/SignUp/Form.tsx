a290614f807bee8390af93dfb53859fb79235b7d
Olamide Atitebi <olamideumarq@gmail.com>
2022-08-22 10:00:36 +0100

    fix: fix typo in signup form

--- components/SignUp/Form.tsx
+++ components/SignUp/Form.tsx
@@ -3,7 +3,7 @@ import { supabase } from '../../utils/supabaseClient';
 
 export const SignUpForm = () => {
 	const [email, setEmail] = useState('');
-	const [password, setPassowrd] = useState('');
+	const [password, setPassword] = useState('');
 	const handleSignup = async () => {
 		const { user, error } = await supabase.auth.signUp({ email, password });
 		if (error) alert(error.message);
@@ -46,7 +46,7 @@ export const SignUpForm = () => {
 					className='relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
 					placeholder='*****'
 					value={password}
-					onChange={e => setPassowrd(e.target.value)}
+					onChange={e => setPassword(e.target.value)}
 				/>
 			</div>
 			<button
