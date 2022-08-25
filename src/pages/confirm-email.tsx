449c09d713c5be471a88e8d1283104f6ead04dd3
Shubham Parihar <shubhamparihar391@gmail.com>
2022-08-26 00:49:59 +0530

    refactor: use react-icons andopen social links in new tab (#128)

--- src/pages/confirm-email.tsx
+++ src/pages/confirm-email.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { type NextPage } from 'next';
+import { MdOutlineMarkEmailRead } from 'react-icons/md';
 
 const ConfirmEmail: NextPage = () => {
 	return (
@@ -9,13 +9,11 @@ const ConfirmEmail: NextPage = () => {
 				<title>defaang | Confirm email</title>
 			</Head>
 			<main className='flex h-full min-h-screen items-center justify-center bg-gray-50'>
-				<div className='grid w-full max-w-xs items-center justify-center rounded-lg bg-white p-6 shadow-lg sm:max-w-md'>
-					<div className='relative mx-auto aspect-square w-full max-w-xs '>
-						<Image src='/welcome.svg' alt='envelope' layout='fill' className='object-cover object-center'></Image>
-					</div>
-					<div className='text-center text-gray-700'>
-						<p>Thanks for signing up! We&#39;ve sent you an email to confirm your account.</p>
-					</div>
+				<div className='grid w-full max-w-xs grid-cols-1 justify-items-center gap-y-4 rounded-lg bg-white p-6 shadow-lg sm:max-w-md'>
+					<MdOutlineMarkEmailRead title='envelope' className='h-28 w-28 fill-blue-500' />
+					<p className='text-center text-gray-700'>
+						Thanks for signing up! We&#39;ve sent you an email to confirm your account.
+					</p>
 				</div>
 			</main>
 		</>
