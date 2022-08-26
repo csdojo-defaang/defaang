46f940dc54dfa676586bba999bc9faf94831a1ca
Shubham Parihar <shubhamparihar391@gmail.com>
2022-08-26 21:29:23 +0530

    refactor: use title tag in pages instead of _document (#129)
    * refactor: use title tag in pages instead of _document
    
    * refactor: use lowercase for Defaang and add tagline

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,19 +4,18 @@ export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
-				<title>welcome to defaang</title>
 				<link rel='icon' href='/favicon.ico' />
 				<link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
 				<meta name='description' content='// TODO: website description' />
 				{/* Open Graph Meta Tags */}
-				<meta property='og:title' content='Defaang' />
+				<meta property='og:title' content='defaang' />
 				<meta property='og:description' content='// TODO: website description' />
 				<meta property='og:type' content='website' />
 				<meta property='og:image' content='// TODO: logo.png' />
 				{/* Theme Color Meta Tags */}
 				<meta name='theme-color' content='// TODO: color' />
 				{/* Twitter Meta Tags  */}
-				<meta name='twitter:title' content='Defaang' />
+				<meta name='twitter:title' content='defaang' />
 				<meta name='twitter:description' content='// TODO: website description' />
 				<meta name='twitter:image' content='// TODO: https://root-domain-name/logo.png' />
 				<meta name='twitter:card' content='summary' />
