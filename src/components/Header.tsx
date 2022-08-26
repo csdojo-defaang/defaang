53007ecadbe6ca945aba33bef96e4a3cf9e6c88a
omkarg01 <omkargujja01@gmail.com>
2022-08-26 23:56:30 +0530

    feat: header-tailwind-ui added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,164 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import SignIn from '../pages/signin';
 import Link from 'next/link';
-import { FaDiscord, FaGithub, FaTwitter } from 'react-icons/fa';
+
+function classNames(...classes: string[]) {
+	return classes.filter(Boolean).join(' ');
+}
 
 export function Header() {
 	return (
-		<nav className='flex items-center justify-between py-4 shadow-[rgba(17,17,26,0.1)_0px_1px_0px] lg:pl-[20%] lg:pr-[20%]'>
-			<Link href='/'>
-				<a>
-					<span className='cursor-pointer px-5 text-3xl font-bold text-gray-700'>defaang</span>
-				</a>
-			</Link>
-			<div className='mx-5 flex w-1/3 items-center justify-between lg:w-1/6'>
-				<Link href='https://github.com/ykdojo/defaang'>
-					<a target='_blank' rel='noopener noreferrer'>
-						<FaGithub className='h-8 w-8' title='GitHub' />
-					</a>
-				</Link>
-				<Link href='https://discord.gg/aJp6Fypb'>
-					<a target='_blank' rel='noopener noreferrer'>
-						<FaDiscord className='h-8 w-8' title='Discord' />
-					</a>
-				</Link>
-				<Link href='https://twitter.com/ykdojo'>
-					<a target='_blank' rel='noopener noreferrer'>
-						<FaTwitter className='h-8 w-8' title='Twitter' />
-					</a>
-				</Link>
-			</div>
-		</nav>
+		<Disclosure as='nav' className='bg-gray-800'>
+			{({ open }) => (
+				<>
+					<div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+						<div className='flex h-16 items-center justify-between'>
+							<div className='flex items-center'>
+								<div className='flex-shrink-0'>
+									<img
+										className='block h-8 w-auto lg:hidden'
+										src='https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=500'
+										alt='Workflow'
+									/>
+									<img
+										className='hidden h-8 w-auto lg:block'
+										src='https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=500'
+										alt='Workflow'
+									/>
+								</div>
+								<div className='hidden sm:ml-6 sm:block'>
+									<div className='flex space-x-4'>
+										{/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+										<a href='#' className='rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white'>
+											Dashboard
+										</a>
+										<a
+											href='#'
+											className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+										>
+											Team
+										</a>
+										<a
+											href='#'
+											className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+										>
+											Projects
+										</a>
+										<a
+											href='#'
+											className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+										>
+											Calendar
+										</a>
+									</div>
+								</div>
+							</div>
+							<div className='hidden sm:ml-6 sm:block'>
+								<div className='flex items-center'>
+									<Link href='#'>
+										<a className='text-white' target='_blank' rel='noopener noreferrer'>
+											Sign In
+										</a>
+									</Link>
+								</div>
+							</div>
+							<div className='-mr-2 flex sm:hidden'>
+								{/* Mobile menu button */}
+								<Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+									<span className='sr-only'>Open main menu</span>
+									{open ? (
+										<XIcon className='block h-6 w-6' aria-hidden='true' />
+									) : (
+										<MenuIcon className='block h-6 w-6' aria-hidden='true' />
+									)}
+								</Disclosure.Button>
+							</div>
+						</div>
+					</div>
+
+					<Disclosure.Panel className='sm:hidden'>
+						<div className='space-y-1 px-2 pt-2 pb-3'>
+							{/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+							<Disclosure.Button
+								as='a'
+								href='#'
+								className='block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white'
+							>
+								Dashboard
+							</Disclosure.Button>
+							<Disclosure.Button
+								as='a'
+								href='#'
+								className='block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+							>
+								Team
+							</Disclosure.Button>
+							<Disclosure.Button
+								as='a'
+								href='#'
+								className='block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+							>
+								Projects
+							</Disclosure.Button>
+							<Disclosure.Button
+								as='a'
+								href='#'
+								className='block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+							>
+								Calendar
+							</Disclosure.Button>
+						</div>
+						<div className='border-t border-gray-700 pt-4 pb-3'>
+							<div className='flex items-center px-5'>
+								<div className='flex-shrink-0'>
+									<img
+										className='h-10 w-10 rounded-full'
+										src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+										alt=''
+									/>
+								</div>
+								<div className='ml-3'>
+									<div className='text-base font-medium text-white'>Tom Cook</div>
+									<div className='text-sm font-medium text-gray-400'>tom@example.com</div>
+								</div>
+								<button
+									type='button'
+									className='ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+								>
+									<span className='sr-only'>View notifications</span>
+									<BellIcon className='h-6 w-6' aria-hidden='true' />
+								</button>
+							</div>
+							<div className='mt-3 space-y-1 px-2'>
+								<Disclosure.Button
+									as='a'
+									href='#'
+									className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
+								>
+									Your Profile
+								</Disclosure.Button>
+								<Disclosure.Button
+									as='a'
+									href='#'
+									className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
+								>
+									Settings
+								</Disclosure.Button>
+								<Disclosure.Button
+									as='a'
+									href='#'
+									className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
+								>
+									Sign out
+								</Disclosure.Button>
+							</div>
+						</div>
+					</Disclosure.Panel>
+				</>
+			)}
+		</Disclosure>
 	);
 }
