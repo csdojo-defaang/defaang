92e115a6013e40f3feb8f4ed49d43d236e2487c5
omkarg01 <omkargujja01@gmail.com>
2022-08-28 00:12:46 +0530

    feat: added header profile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-import { Disclosure } from '@headlessui/react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { HiBell, HiMenu, HiX } from 'react-icons/hi';
-import Link from 'next/link';
+import { Fragment } from 'react';
 
 function classNames(...classes: string[]) {
 	return classes.filter(Boolean).join(' ');
@@ -54,11 +54,74 @@ export function Header() {
 							</div>
 							<div className='hidden sm:ml-6 sm:block'>
 								<div className='flex items-center'>
-									<Link href='/signin'>
-										<a className='text-white' target='_blank' rel='noopener noreferrer'>
-											Sign In
-										</a>
-									</Link>
+									<button
+										type='button'
+										className='rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+									>
+										<span className='sr-only'>View notifications</span>
+										<HiBell className='h-6 w-6' aria-hidden='true' />
+									</button>
+
+									{/* Profile dropdown */}
+									<Menu as='div' className='relative ml-3'>
+										<div>
+											<Menu.Button className='focus:underline-2 focus:underline-none flex rounded-full bg-gray-800 text-sm'>
+												<span className='sr-only'>Open user menu</span>
+												<div className='text-base font-medium text-gray-300 hover:text-white'>Tom Cook</div>
+											</Menu.Button>
+										</div>
+										<Transition
+											as={Fragment}
+											enter='transition ease-out duration-100'
+											enterFrom='transform opacity-0 scale-95'
+											enterTo='transform opacity-100 scale-100'
+											leave='transition ease-in duration-75'
+											leaveFrom='transform opacity-100 scale-100'
+											leaveTo='transform opacity-0 scale-95'
+										>
+											<Menu.Items className='absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+												<Menu.Item>
+													{({ active }) => (
+														<a
+															href='#'
+															className={classNames(
+																active ? 'bg-gray-100' : '',
+																'block px-4 py-2 text-sm text-gray-700',
+															)}
+														>
+															Your Profile
+														</a>
+													)}
+												</Menu.Item>
+												<Menu.Item>
+													{({ active }) => (
+														<a
+															href='#'
+															className={classNames(
+																active ? 'bg-gray-100' : '',
+																'block px-4 py-2 text-sm text-gray-700',
+															)}
+														>
+															Settings
+														</a>
+													)}
+												</Menu.Item>
+												<Menu.Item>
+													{({ active }) => (
+														<a
+															href='#'
+															className={classNames(
+																active ? 'bg-gray-100' : '',
+																'block px-4 py-2 text-sm text-gray-700',
+															)}
+														>
+															Sign out
+														</a>
+													)}
+												</Menu.Item>
+											</Menu.Items>
+										</Transition>
+									</Menu>
 								</div>
 							</div>
 							<div className='-mr-2 flex sm:hidden'>
