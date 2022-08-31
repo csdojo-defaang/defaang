dbe791d0d5a56fff37c8259bdb46f1bfe491764e
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2022-08-31 21:10:56 +0900

    feat: improved header menu component
    - Divided menu into sections with visual divider
    - Added 'Signed in as <email>' item to the top list

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -90,46 +90,57 @@ export function Header({ user }: UserProps) {
 													leaveFrom='transform opacity-100 scale-100'
 													leaveTo='transform opacity-0 scale-95'
 												>
-													<Menu.Items className='absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-														<Menu.Item>
-															{({ active }) => (
-																<a
-																	href='#'
-																	className={classNames(
-																		active ? 'bg-gray-100' : '',
-																		'block px-4 py-2 text-sm text-gray-700',
-																	)}
-																>
-																	Your Profile
-																</a>
-															)}
-														</Menu.Item>
-														<Menu.Item>
-															{({ active }) => (
-																<a
-																	href='#'
-																	className={classNames(
-																		active ? 'bg-gray-100' : '',
-																		'block px-4 py-2 text-sm text-gray-700',
-																	)}
-																>
-																	Settings
-																</a>
-															)}
-														</Menu.Item>
-														<Menu.Item>
-															{({ active }) => (
-																<a
-																	href='#'
-																	className={classNames(
-																		active ? 'bg-gray-100' : '',
-																		'block px-4 py-2 text-sm text-gray-700',
-																	)}
-																>
-																	Sign out
-																</a>
-															)}
-														</Menu.Item>
+													<Menu.Items className='absolute right-0 mt-2 w-48 origin-top-right divide-y rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+														<Menu.Items>
+															<Menu.Item>
+																<span className='block cursor-default px-4 py-2 text-sm text-gray-700'>
+																	Signed in as <b>tom@example.com</b>
+																</span>
+															</Menu.Item>
+														</Menu.Items>
+														<Menu.Items>
+															<Menu.Item>
+																{({ active }) => (
+																	<a
+																		href='#'
+																		className={classNames(
+																			active ? 'bg-gray-100' : '',
+																			'block px-4 py-2 text-sm text-gray-700',
+																		)}
+																	>
+																		Your Profile
+																	</a>
+																)}
+															</Menu.Item>
+															<Menu.Item>
+																{({ active }) => (
+																	<a
+																		href='#'
+																		className={classNames(
+																			active ? 'bg-gray-100' : '',
+																			'block px-4 py-2 text-sm text-gray-700',
+																		)}
+																	>
+																		Settings
+																	</a>
+																)}
+															</Menu.Item>
+														</Menu.Items>
+														<Menu.Items>
+															<Menu.Item>
+																{({ active }) => (
+																	<a
+																		href='#'
+																		className={classNames(
+																			active ? 'bg-gray-100' : '',
+																			'block px-4 py-2 text-sm text-gray-700',
+																		)}
+																	>
+																		Sign out
+																	</a>
+																)}
+															</Menu.Item>
+														</Menu.Items>
 													</Menu.Items>
 												</Transition>
 											</Menu>
