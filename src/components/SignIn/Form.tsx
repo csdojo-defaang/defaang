1d600a55832bde010814e6e345fa9b5b931e9e92
Shubham Parihar <shubhamparihar391@gmail.com>
2022-09-04 19:37:03 +0530

    feat: add show-hide password button (#153)
    * feat: add show-hide password button
    
    * style: add fill-gray-500 to eye icons

--- src/components/SignIn/Form.tsx
+++ src/components/SignIn/Form.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { supabase } from '../../utils/supabaseClient';
-import { FaLock } from 'react-icons/fa';
+import { HiEye, HiEyeOff } from 'react-icons/hi';
 
 export function SignInForm() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [showPassword, setShowPassword] = useState(false);
 
 	const handleSignIn = async () => {
 		const { user, error } = await supabase.auth.signIn({ email, password });
@@ -18,14 +19,14 @@ export function SignInForm() {
 
 	return (
 		<form
-			className='mt-4 space-y-4'
+			className='mt-4 flex flex-col space-y-4'
 			onSubmit={e => {
 				e.preventDefault();
 				handleSignIn();
 			}}
 		>
 			{/* TODO: allow both username and email in this input */}
-			<div className='flex w-full flex-col space-y-3'>
+			<div className='flex w-full flex-col space-y-2'>
 				<label htmlFor='email' className='text-sm text-gray-600'>
 					Email address
 				</label>
@@ -41,17 +42,30 @@ export function SignInForm() {
 			</div>
 			<div className='flex w-full flex-col space-y-2 '>
 				<label htmlFor='password' className='text-sm text-gray-600'>
-					password
+					Password
 				</label>
-				<input
-					type='password'
-					id='password'
-					autoComplete='current-password'
-					required
-					className='relative flex w-full appearance-none justify-center rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
-					value={password}
-					onChange={e => setPassword(e.target.value)}
-				/>
+				<div className='flex'>
+					<input
+						type={showPassword ? 'text' : 'password'}
+						id='password'
+						autoComplete='current-password'
+						required
+						className='relative block w-full appearance-none rounded-l-md border border-r-0 border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+					/>
+					<button
+						className='rounded-r-md border border-l-0 border-gray-300 px-2 focus:outline-none'
+						type='button'
+						onClick={() => setShowPassword(!showPassword)}
+					>
+						{showPassword ? (
+							<HiEye className='h-4 w-4 fill-gray-500' title='hide password' />
+						) : (
+							<HiEyeOff className='h-4 w-4 fill-gray-500' title='show password' />
+						)}
+					</button>
+				</div>
 			</div>
 			{/*TODO: REMEMBER ME AND FORGOT PASSWORD FUNCTIONALITY */}
 			{/* <div className='flex items-center justify-between'>
@@ -79,9 +93,6 @@ export function SignInForm() {
 				type='submit'
 				className='group relative my-4 flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
 			>
-				<span className='absolute inset-y-0 left-0 flex items-center pl-3'>
-					<FaLock className='text-base text-indigo-500 group-hover:text-indigo-400' />
-				</span>
 				Sign in
 			</button>
 		</form>
