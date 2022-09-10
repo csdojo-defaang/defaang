165ff33c7d4ccf6120311c7c154476606fd4fa57
Shubham Parihar <shubhamparihar391@gmail.com>
2022-09-10 13:43:54 +0530

    feat: add input fields for asked month and year

--- src/components/QuestionSubmissionForm.tsx
+++ src/components/QuestionSubmissionForm.tsx
@@ -4,15 +4,31 @@ interface QuestionSubmissionFormInputs {
 	company: string;
 	position: string;
 	location: string;
-	asked_date: Date;
+	asked_month: string;
+	asked_year: number;
 	question: string;
 	question_details: string;
 	stay_anonymous: boolean;
 }
 
+const months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+] as const;
+
 export function QuestionSubmissionForm() {
 	// reference: https://react-hook-form.com/get-started#Quickstart
-	const { register, handleSubmit, reset } = useForm<QuestionSubmissionFormInputs>();
+	const { register, handleSubmit, reset, formState } = useForm<QuestionSubmissionFormInputs>();
 
 	const onSubmit: SubmitHandler<QuestionSubmissionFormInputs> = data => {
 		console.log(data);
@@ -20,7 +36,7 @@ export function QuestionSubmissionForm() {
 	};
 
 	return (
-		<div className='w-full bg-white p-4 shadow-lg sm:w-4/5 md:w-2/3 lg:w-1/2'>
+		<div className='w-full rounded-md bg-white p-4 shadow-md sm:w-4/5 md:w-2/3 lg:w-1/2'>
 			<form className='space-y-8 divide-y divide-gray-200' onSubmit={handleSubmit(onSubmit)}>
 				<div className='space-y-8 divide-y divide-gray-200'>
 					<div>
@@ -29,73 +45,108 @@ export function QuestionSubmissionForm() {
 						</div>
 
 						<div className='mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6'>
-							<div className='sm:col-span-4'>
+							<div className='sm:col-span-6'>
 								<label htmlFor='company' className='block text-sm font-medium text-gray-700'>
 									Company
 								</label>
-								<div className='mt-1 flex rounded-md shadow-sm'>
+								<div className='mt-1 flex flex-col gap-y-1'>
 									<input
 										type='text'
 										id='company'
 										className='block w-full min-w-0 flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
 										{...register('company', { required: true })}
 									/>
+									{formState.errors.company && <span className='text-xs text-red-500'>Company is required</span>}
 								</div>
 							</div>
 
-							<div className='sm:col-span-4'>
+							<div className='sm:col-span-6'>
 								<label htmlFor='position' className='block text-sm font-medium text-gray-700'>
 									Position
 								</label>
-								<div className='mt-1 flex rounded-md shadow-sm'>
+								<div className='mt-1 flex flex-col gap-y-1'>
 									<input
 										type='text'
 										{...register('position', { required: true })}
 										id='position'
 										className='block w-full min-w-0 flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
 									/>
+									{formState.errors.position && <span className='text-xs text-red-500'>Position is required</span>}
 								</div>
 							</div>
 
-							<div className='sm:col-span-4'>
+							<div className='sm:col-span-6'>
 								<label htmlFor='location' className='block text-sm font-medium text-gray-700'>
 									Location
 								</label>
-								<div className='mt-1 flex rounded-md shadow-sm'>
+								<div className='mt-1 flex flex-col gap-y-1'>
 									<input
 										type='text'
 										{...register('location', { required: true })}
 										id='location'
 										className='block w-full min-w-0 flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
 									/>
+									{formState.errors.location && <span className='text-xs text-red-500'>Location is required</span>}
 								</div>
 							</div>
 
-							<div className='sm:col-span-3'>
-								<label htmlFor='asked-date' className='block text-sm font-medium text-gray-700'>
-									When was this asked?
-								</label>
-								<div className='mt-1'>
-									<input
-										type='date'
-										{...register('asked_date', { required: true })}
-										id='asked-date'
-										className='block w-full min-w-0 flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-									/>
+							<fieldset className='sm:col-span-6'>
+								<legend>When was this asked?</legend>
+								<div className='mt-2 flex flex-row gap-x-1'>
+									<div className='w-2/3 flex-auto'>
+										<label htmlFor='asked-month' className='block text-xs font-medium text-gray-500'>
+											Month
+										</label>
+										<div className='mt-1 flex flex-col gap-y-1'>
+											<select
+												{...register('asked_month', { required: true })}
+												id='asked-month'
+												className='block w-full min-w-0 flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+											>
+												<option hidden={true} value=''>
+													Select
+												</option>
+												{months.map(m => (
+													<option key={m}>{m}</option>
+												))}
+											</select>
+											{formState.errors.asked_month && <span className='text-xs text-red-500'>Month is required</span>}
+										</div>
+									</div>
+									<div className='w-1/3 flex-auto'>
+										<label htmlFor='asked-year' className='block text-xs font-medium text-gray-500'>
+											Year
+										</label>
+										<div className='mt-1 flex flex-col gap-y-1'>
+											<input
+												type='tel'
+												{...register('asked_year', { required: true, pattern: /^\d{4}$/, valueAsNumber: true })}
+												id='asked-year'
+												className='block w-full min-w-0 flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+											/>
+											{formState.errors.asked_year && formState.errors.asked_year.type === 'required' && (
+												<span className='text-xs text-red-500'>Year is required</span>
+											)}
+											{formState.errors.asked_year && formState.errors.asked_year.type === 'pattern' && (
+												<span className='text-xs text-red-500'>Not a valid year</span>
+											)}
+										</div>
+									</div>
 								</div>
-							</div>
+							</fieldset>
 
 							<div className='sm:col-span-6'>
 								<label htmlFor='question' className='block text-sm font-medium text-gray-700'>
 									Question
 								</label>
-								<div className='mt-1 flex rounded-md shadow-sm'>
+								<div className='mt-1 flex flex-col gap-y-1'>
 									<input
 										type='text'
 										{...register('question', { required: true })}
 										id='question'
 										className='block w-full min-w-0 flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
 									/>
+									{formState.errors.question && <span className='text-xs text-red-500'>Question is required</span>}
 								</div>
 							</div>
 
