673b7899d6f0a50632e1d170fc91cdf1d688d640
ykdojo <yksu4p@gmail.com>
2022-09-12 18:17:14 -0700

    trying to set up the new header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import Head from 'next/head';
-import { Header } from '../components/Header';
-import type { PageProps } from '../lib/types';
+// import { Header } from '../components/Header';
+import { Header2 } from '../components/Header2';
 import { PlaceHolder } from '../components/PlaceHolder';
 import { Footer } from '../components/Footer';
 
-export default function Home({ user }: PageProps) {
+export default function Home() {
 	return (
 		<>
 			<Head>
 				<title>{'defaang: recently-asked interview questions at FAANG+, collected & curated'}</title>
 			</Head>
 
-			<Header user={user} />
+			{/* <Header user={user} /> */}
+			<Header2 />
 			<main className='flex min-h-screen flex-col items-center justify-center px-[5%] text-center lg:px-[20%]'>
 				<PlaceHolder />
 			</main>
