d340bff96831a5443d9c2bc38a35d6653316fc9b
ykdojo <yksu4p@gmail.com>
2022-09-12 19:41:08 -0700

    added header in the landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,20 @@
 import Head from 'next/head';
-import { Header } from '../components/Header';
-import type { PageProps } from '../lib/types';
-import { PlaceHolder } from '../components/PlaceHolder';
-import { Footer } from '../components/Footer';
+import { Header2 } from '../components/Header2';
+// import { PlaceHolder } from '../components/PlaceHolder';
+// import { Footer } from '../components/Footer';
 
-export default function Home({ user }: PageProps) {
+export default function Home() {
 	return (
 		<>
 			<Head>
 				<title>{'defaang: recently-asked interview questions at FAANG+, collected & curated'}</title>
 			</Head>
 
-			<Header user={user} />
-			<main className='flex min-h-screen flex-col items-center justify-center px-[5%] text-center lg:px-[20%]'>
+			<Header2 />
+			{/* <main className='flex min-h-screen flex-col items-center justify-center px-[5%] text-center lg:px-[20%]'>
 				<PlaceHolder />
 			</main>
-			<Footer />
+			<Footer /> */}
 		</>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@
 
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-	content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}',
+		'./src/pages/*.{js,ts,jsx,tsx}',
+		'./src/components/*.{js,ts,jsx,tsx}',
+	],
 	theme: {
 		extend: {
 			fontFamily: {
