1dccf47486760bc64ebbb7722924bfae8640b011
Prateek Singh <devXprite@gmail.com>
2022-10-04 01:45:41 +0000

    Update meta tags in _document.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,36 +10,7 @@ export default function Home({ session }: PageProps) {
   return (
     <>
       <Head>
-        <title>Defaang</title>
-        <meta name='title' content='Defaang' />
-        <meta
-          name='description'
-          content='A website that will curate recently-asked interview questions from FAANG+ to help people practice and prep!'
-        />
-
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://defaang.io/' />
-        <meta property='og:title' content='Defaang' />
-        <meta
-          property='og:description'
-          content='A website that will curate recently-asked interview questions from FAANG+ to help people practice and prep!'
-        />
-        <meta
-          property='og:image'
-          content='https://user-images.githubusercontent.com/1811651/190090988-bd062b3d-635b-4ce1-ac15-81d3c9edcb91.png'
-        />
-
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta property='twitter:url' content='https://defaang.io/' />
-        <meta property='twitter:title' content='Defaang' />
-        <meta
-          property='twitter:description'
-          content='A website that will curate recently-asked interview questions from FAANG+ to help people practice and prep!'
-        />
-        <meta
-          property='twitter:image'
-          content='https://user-images.githubusercontent.com/1811651/190090988-bd062b3d-635b-4ce1-ac15-81d3c9edcb91.png'
-        />
+        <title>{'defaang: recently-asked interview questions at FAANG+, collected & curated'}</title>
       </Head>
 
       <div className="bg-[url('/hero-background.jpg')] bg-top">
