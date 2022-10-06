8dc151c3ea822425b21456ed5c2dea60c2305c56
CamilingJS <jonathancamiling@gmail.com>
2022-10-05 23:59:50 -0700

    added logos into images folder and updated favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/dflogo.svg' />
         <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
