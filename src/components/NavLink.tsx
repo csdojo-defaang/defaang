81cf3d68b157bbba77ed4cd8516cc2a6dad85bda
JuanMOlivas <41582680+CerealPlayer@users.noreply.github.com>
2022-10-07 12:25:40 +0200

    dx: use more named imports
    Co-authored-by: Subho <108616679+subhoghoshX@users.noreply.github.com>

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { ReactNode } from 'react';
 
-export function NavLink({ href, children }: { href: string; children: React.ReactNode }) {
+export function NavLink({ href, children }: { href: string; children: ReactNode }) {
   return (
     <Link
       href={href}
