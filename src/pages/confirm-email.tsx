1c2f045baa82d15375ae9b49f75226059262ef15
unknown <abdelpro1236@gmail.com>
2022-10-09 22:54:25 +0100

    bug fix issue #215

--- src/pages/confirm-email.tsx
+++ src/pages/confirm-email.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import { MdOutlineMarkEmailRead } from 'react-icons/md';
 import type { PageProps } from '../lib/types';
 
@@ -15,6 +16,11 @@ export default function ConfirmEmail({}: PageProps) {
           <p className='text-center text-gray-700'>
             Thanks for signing up! We&#39;ve sent you an email to confirm your account.
           </p>
+          <Link href='/signin'>
+            <a className='group relative my-4 flex w-full justify-center rounded-md border border-transparent bg-blue-500 py-2 px-4 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
+              Go to Sign in
+            </a>
+          </Link>
         </div>
       </main>
     </>
