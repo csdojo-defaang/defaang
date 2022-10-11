f8bf6fee01ed0fac6fae89fa433873f9bbde1473
Younes Barrad <younesbarraddev@gmail.com>
2022-10-11 22:02:13 +0100

    fix: Fixed JS file and question submission

--- src/pages/api/submit_question.ts
+++ src/pages/api/submit_question.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createClient } from '@supabase/supabase-js';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 // TODO: move the following somewhere else?
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
@@ -8,7 +9,7 @@ const supabaseServiceKey = process.env.NEXT_PRIVATE_SUPABASE_SERVICE_KEY || '';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 const supabaseSecret = createClient(supabaseUrl, supabaseServiceKey);
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // input_data format:
   // {
   // 	company: "a",
@@ -23,18 +24,25 @@ export default async function handler(req, res) {
   // NOTE: recency_weeks: "2" means we estimate this question was asked about 2 weeks ago.
 
   const input_data = JSON.parse(req.body);
-  const jwt = req.headers.authentication;
-  const asked_date = new Date(new Date().setDate(new Date().getDate() - Number(input_data['recency_weeks']) * 7));
+  const jwt = req.headers.authentication as string;
+  const estimated_asked_date = new Date(
+    new Date().setDate(new Date().getDate() - Number(input_data['recency_weeks']) * 7),
+  );
 
-  input_data['asked_date'] = asked_date;
+  input_data['estimated_asked_date'] = estimated_asked_date;
   delete input_data['recency_weeks'];
 
+  if (!jwt || jwt.length == 0) return res.status(401).json({ error: 'Unauthorized' });
+
   const { data: user } = await supabase.auth.api.getUser(jwt);
+
+  if (!user || !user.identities || user.identities.length == 0) return res.status(401).json({ error: 'Unauthorized' });
+
   const id = user.identities[0]['id'];
 
   input_data['created_by'] = id;
 
-  const { data, error } = await supabaseSecret.from('questions').insert([input_data]);
+  const { error } = await supabaseSecret.from('questions').insert([input_data]);
 
   if (error) {
     res.status(500).json({ error });
