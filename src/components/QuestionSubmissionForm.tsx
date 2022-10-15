bb95e3b1e523f0ac3066f6f8d455c3154caa9c0b
Akshay Kumar <62614172+Akshay1018@users.noreply.github.com>
2022-10-15 21:30:49 +0530

    feat: Better question submission flow (#278)
    * Added success popup on creating new question
    
    * Updated popup message
    
    * changes: Removed fragments from questionForm file.

--- src/components/QuestionSubmissionForm.tsx
+++ src/components/QuestionSubmissionForm.tsx
@@ -1,4 +1,8 @@
+import React, { useState, Fragment } from 'react';
 import { type SubmitHandler, useForm, FieldValues } from 'react-hook-form';
+
+//3rd party components
+import { Dialog, Transition } from '@headlessui/react';
 import type { PageProps } from '../lib/types';
 
 interface QuestionSubmissionFormInputs {
@@ -12,17 +16,29 @@ interface QuestionSubmissionFormInputs {
 }
 
 export function QuestionSubmissionForm({ session }: PageProps) {
+  const [state, setState] = useState({
+    showPopup: false,
+    btnLoading: false,
+  });
   // reference: https://react-hook-form.com/get-started#Quickstart
   const { register, handleSubmit, reset, formState } = useForm<QuestionSubmissionFormInputs>();
 
+  // Modal Popup
+  const handleModalPopup = () => {
+    setState(prevState => ({
+      ...prevState,
+      showPopup: !state.showPopup,
+    }));
+  };
+
   const onSubmit: SubmitHandler<FieldValues> = async data => {
-    console.log(data);
-
     if (session === undefined || session === null) {
-      console.log('user not logged in.');
-
       return;
     }
+    setState(prevState => ({
+      ...prevState,
+      btnLoading: true,
+    }));
 
     let result;
     try {
@@ -33,12 +49,16 @@ export function QuestionSubmissionForm({ session }: PageProps) {
         method: 'POST',
         body: JSON.stringify(data),
       });
+      if (result?.status === 200) {
+        setState(prevState => ({
+          ...prevState,
+          btnLoading: false,
+        }));
+        handleModalPopup();
+      }
     } catch (err) {
       console.log(err);
     }
-
-    console.log(result);
-
     reset();
   };
 
@@ -171,11 +191,59 @@ export function QuestionSubmissionForm({ session }: PageProps) {
               type='submit'
               className='ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
             >
-              Submit
+              {state.btnLoading ? 'Loading...' : 'Submit'}
             </button>
           </div>
         </div>
       </form>
+      <Transition appear show={state.showPopup} as={Fragment}>
+        <Dialog as='div' className='relative z-10' onClose={handleModalPopup}>
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='fixed inset-0 bg-black bg-opacity-25' />
+          </Transition.Child>
+
+          <div className='fixed inset-0 overflow-y-auto'>
+            <div className='flex min-h-full items-center justify-center p-4 text-center'>
+              <Transition.Child
+                as={Fragment}
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
+              >
+                <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+                  <Dialog.Title as='h3' className='text-lg font-medium leading-6 text-gray-900'>
+                    Question submitted successfully!
+                  </Dialog.Title>
+                  <div className='mt-2'>
+                    <p className='text-sm text-gray-500'>Thank you! We will review the question as soon as possible.</p>
+                  </div>
+
+                  <div className='mt-4'>
+                    <button
+                      type='button'
+                      className='float-right inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
+                      onClick={handleModalPopup}
+                    >
+                      Add more?
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   );
 }
