8336c7c4fb22f26eb69aa742aa8f5e5a5c7d3263
ykdojo <yksu4p@gmail.com>
2022-10-15 09:48:26 -0700

    Add description about next milestone

--- src/components/QuestionApprovalProgressBarSection.tsx
+++ src/components/QuestionApprovalProgressBarSection.tsx
@@ -22,7 +22,7 @@ export function QuestionApprovalProgressBarSection() {
         </div>
       </div>
       <div className='mt-2 font-semibold text-slate-700 sm:hidden'>approved questions</div>
-      <div className='relative mx-auto my-10 h-6 w-4/5 overflow-x-hidden rounded-full bg-gray-300'>
+      <div className='relative mx-auto my-10 h-6 w-3/5 overflow-x-hidden rounded-full bg-gray-300'>
         <span
           className='absolute top-0 left-0 h-full min-w-[32px] max-w-full rounded-full bg-gradient-to-r from-blue-500 to-indigo-500 px-2 text-right text-sm text-white transition-all duration-500'
           style={{ width: `${progressBarPercentage}%` }}
@@ -30,6 +30,9 @@ export function QuestionApprovalProgressBarSection() {
           {progressBarPercentage}%
         </span>
       </div>
+      <p className='mx-auto mt-6 max-w-4xl text-lg tracking-tight text-slate-700'>
+        Unlock: you&apos;ll get access to all approved questions once your question has been approved.
+      </p>
     </Container>
   );
 }
