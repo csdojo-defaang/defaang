6b8b2505201cf38372a03d048bd6812d1437cb77
Trecia Kat <tksekgalo@gmail.com>
2022-10-29 11:32:43 +0200

    fix: reduced m-20 to mt-10

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -98,7 +98,7 @@ function Feature({ feature, isActive, className, ...props }: props) {
 
 function FeaturesMobile() {
   return (
-    <div className='-mx-4 mt-20 flex flex-col gap-y-10 overflow-hidden px-4 sm:-mx-6 sm:px-6 lg:hidden'>
+    <div className='-mx-4 mt-10 flex flex-col gap-y-10 overflow-hidden px-4 sm:-mx-6 sm:px-6 lg:hidden'>
       {features.map(feature => (
         <div key={feature.name}>
           <Feature feature={feature} className='mx-auto max-w-2xl' isActive />
